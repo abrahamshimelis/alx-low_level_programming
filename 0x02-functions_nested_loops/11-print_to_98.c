09b67021fce786a74e2da3ee066956c182249c3a
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-05 19:59:05 +0000

    debug question 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		printf("%d\n", 98);
 	} else if (n == 98)
 	{
-		printf("%d \n", n);
+		printf("%d\n", n);
 	} else if (n > 0)
 	{
 		while (n > 98)
@@ -25,6 +25,6 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n--;
 		}
-		printf("%d\n ", 98);
+		printf("%d\n", 98);
 	}
 }
