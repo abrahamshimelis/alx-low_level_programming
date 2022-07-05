77d9541cfa1fdc666fd481100d27852d70f4b9b1
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-05 19:34:37 +0000

    debug question 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic character 
- *
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
  * Return: 1 on success and 0 otherwise
  */
 int _isalpha(int c)
