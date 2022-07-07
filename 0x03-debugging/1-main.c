eda8edf4645048cbf93d884e2227df17d6a8c92c
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-07 14:00:46 +0000

    fix bug infinte loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infine loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incomming :(\n");
+
+	i = 0;
+
+/*	while (i < 10)
+	{
+		putchar(i);
+	} 
+*/
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
