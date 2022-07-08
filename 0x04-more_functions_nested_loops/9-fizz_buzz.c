2173f468739bafad5f3133878dc1aef660c95055
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-08 18:18:11 +0000

    fix fizz buzz bug

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,19 @@ int main(void)
 
 	while (n <= 100)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
 		{
-			if (n % 5 == 0)
-			{
-				printf("FizzBuzz ");
-			}
-
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0 && n % 5 != 0)
+		{
 			printf("Fizz ");
 		}
-		else if (n % 5 == 0)
+		else if (n % 5 == 0 && n % 3 != 0)
 		{
 			printf("Buzz ");
 		}
-		else
+		else if (n % 3 != 0 && n % 5 != 0)
 		{
 			printf("%d ", n);
 		}
