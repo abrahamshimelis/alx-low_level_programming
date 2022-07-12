ecef85ee6c9c2323c6d9a18aef1b6bd18843ae9b
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-12 13:36:20 +0000

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test a function that prints a string, in reverse,
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
