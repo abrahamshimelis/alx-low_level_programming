22dcb40ff92199c7f9e625b52288edc87eda3163
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-13 05:55:12 +0000

    print half string

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test a function that prints a half of a string,
+ * followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
