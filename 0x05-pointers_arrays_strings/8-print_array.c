bf56af1b222ff61e43bce3626a45f3bb07d685c4
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-13 06:39:36 +0000

    debug print numbers in array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,19 +13,15 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-
-	while (i <= n)
+	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
 		{
 			printf("%d", a[i]);
-			i++;
 		}
-		else if (i < n)
+		else
 		{
 			printf(", %d", a[i]);
-			i++;
 		}
 	}
 	printf("\n");
