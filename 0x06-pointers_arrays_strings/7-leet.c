dc261521d5ad284465d841401bae3ef04971eb2a
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-14 06:51:33 +0000

    encoding to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+/**
+ * *leet - encodes a string in 1337
+ * @str: pointer to string
+ *
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+			}
+		}
+	}
+
+	return (str);
+}
