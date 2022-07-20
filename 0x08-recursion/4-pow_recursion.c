eba22ede6c5a735f002ed3df690a91ee8367b523
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-20 13:22:14 +0000

    question 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - returns the value of x raised to
+ * the power of y
+ * @x: integer number
+ * @y: integer power
+ *
+ * Return: -1 if power is negative, result otherwise
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
