02e71e55a04f421172965d58a79abb06e1661d04
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-07-20 13:04:09 +0000

    question 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - returns the factorial of a given number
+ * @n: integer number
+ *
+ * Return: -1 if n is lower than 0, the factorial otherwise
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
