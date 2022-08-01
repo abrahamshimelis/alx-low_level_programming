029526da8aba8609fc4552d1dab4801f9c2ee752
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-08-01 13:15:14 +0000

    debug question 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
@@ -20,10 +25,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 
 #endif /* DOG_H */
