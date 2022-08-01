45a079b97ea4dd201dc5aee41458699740c52105
Abraham Shimelis <abrahamshimelis@gmail.com>
2022-08-01 13:08:22 +0000

    do question 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
 /**
  * struct dog - a data type of dog
  * @name: name of a dog of type char
@@ -18,7 +21,9 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
 #endif /* DOG_H */
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
